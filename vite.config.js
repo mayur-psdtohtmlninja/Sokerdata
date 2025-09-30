957efbea0eb85dbc4580f0ca7d560f0971c67e6e
mayurmanektech <mayur.bhesaniya@manektech.com>
2025-09-30 11:23:36 +0530

    url changes

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/~yagnik/sokerdata/",
+  base: "/",
   plugins: [react()],
   // Optionally, add other configurations as needed (e.g., proxy settings)
 });
